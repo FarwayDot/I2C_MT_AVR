dd58ddf8b274a3c0bf4f4b6a09882d58dc12523d
unknown <jeanfranco_virgo@hotmail.com>
2021-01-13 20:38:19 -0500

    I2C MT funciones listas para utilizaralas. Queda por probar si funcionan correctamente, esperando a terminar el receptor slave

--- main.c
+++ main.c
@@ -35,84 +35,103 @@
 #define SDA					4
 #define SCL					5
 
+//Estado I2C
+#define TWI_OK				0
+#define TWI_ERROR			1
 
 /*Declaración de funciones*/
-void TWI_MT_Start_Condition(void);
-void TWI_MT_Address(uint8_t address, uint8_t r_nw)
-void TWI_MT_Data(uint8_t data);
+uint8_t TWI_MT_Start_Condition(void);
+uint8_t TWI_MT_Address(uint8_t address, uint8_t r_nw);
+uint8_t TWI_MT_Data_Upload(uint8_t data);
 void TWI_MT_Stop_Condition(void);
-void TWI_MT_Re_Start_Condition(void);
+uint8_t TWI_MT_Re_Start_Condition(void);
+uint8_t TWI_MT_Write_Data(uint8_t address, uint8_t n_data, uint8_t Trama[]);
 
 /*Función principal*/
-void main(void)
+int main(void)
 {	
 		
-	//Led solo de indicador
-	DDRB |= (1<<LED1); //Salida
-	PORTB &= ~(1<<LED1); //Low
-	
-	//Iniciamos pines I2C
-	DDR_TW |= (1<<SCL) | (1<<SDA);
-	PORT_TW &= ~((1<<SCL)|(1<<SDA));
-	
 	//Bit Rate Generator Formula 
-	TWBR = (uint8_t)(((F_CPU/(SCL_FREQ_KHZ*1000))-16)/(2*PRESCALER));
+	//TWBR = (uint8_t)(((F_CPU/(SCL_FREQ_KHZ*1000))-16)/(2*PRESCALER));
 	
-	//TWSR se deja como está, prescaler 1 no necesita modificacion, lo demás son para los estados
-		
-    while (1) 
+    while(1) 
     {	
 		
     }
+	
+	return 0;
 }
 
-void TWI_MT_Start_Condition()
+/************************************************************************/
+/* START CONDITION : Inicia el TWI , y devuelve un valor de error si el 
+	estado no es el correcto */
+/************************************************************************/
+
+uint8_t TWI_MT_Start_Condition(void)
 {
+	//Inicializamos el estado del TWI, si cambia, entonces ocurrirá un error
+	uint8_t salida = TWI_OK;
 	
-	/************************************************************************/
-	/* BIT START */
-	/************************************************************************/
-	
-	//Inicialización de la bandera, bit start y habilitamos el i2c
+	//Inicialización de la bandera, bit start y habilitamos el i2c. 
 	TWCR = (1<<TWINT) | (1<<TWSTA) |(1<<TWEN);
 	
-	//Bandera para el START
+	//Checkeamos si la bandera se activa
 	while(!(TWCR & (1<<TWINT)));
 	
 	//Checkeamos el estado según la tabla del datasheet
 	if((TWSR & 0xF8) != STA_START)
-	{
-		PORTB |= (1<<LED1);
+	{	
+		//Cambiamos el estado del TWI
+		salida = TWI_ERROR;
 	}
 	
+	return salida;	
 }
 
-void TWI_MT_Address(uint8_t address, uint8_t r_nw)
+
+/************************************************************************/
+/* DIRECCION Y R/nW: Carga la dirección(7bits) y si se quiere leer o escribir 
+Nota: No se actualiza aún si se quiere leer*/
+/************************************************************************/
+
+uint8_t TWI_MT_Address(uint8_t address, uint8_t r_nw)
 {
 	
-	/************************************************************************/
-	/* DIRECCION Y R/nW */
-	/************************************************************************/
-	//Cargamos el valor de la dirección(1) y write(0)
-	TWDR = (address<<1) | r_nw ;
-	//Según datasheet se limpia la bandera después de cargar el valor 
+	uint8_t salida = TWI_OK;
+	
+	//Cargamos el valor de la dirección(1er) y write(2do)
+	if(r_nw)
+	{
+		salida = TWI_ERROR;
+	}
+	else
+	{	
+		TWDR = (address<<1) & 0xFE ;
+	}
+	
+	//Según datasheet se limpia la bandera después de cargar el valor
 	TWCR = (1<<TWINT) | (1<<TWEN);
 	
+	//Checkeamos si la bandera se activa
 	while(!(TWCR & (1<<TWINT))); 
 		
+	//Checkeamos si es el estado correcto
 	if((TWSR & 0xF8) != STA_SLA_W_ACK)
 	{
-		PORTB |= (1<<LED1);
+		salida = TWI_ERROR;
 	}
 	
+	return salida;
 }
 
-void TWI_MT_Data(uint8_t data)
+/************************************************************************/
+/* DATA (8bits): Cargar data de 8 bits*/
+/************************************************************************/
+
+
+uint8_t TWI_MT_Data_Upload(uint8_t data)
 {
-	
-	/************************************************************************/
-	/* DATA (8bits) */
-	/************************************************************************/
+	uint8_t salida = TWI_OK;
 	
 	//Cargamos el valor de la data, nuestro caso, letra 'A'
 	TWDR = data;
@@ -122,19 +141,22 @@ void TWI_MT_Data(uint8_t data)
 	//Esperamos la bander por enviar la data
 	while(!(TWCR & (1<<TWINT)));
 	
-	//Checkeamos si es la bandera del ACK del receptor
+	 //Checkeamos si es la bandera del ACK del receptor
 	if((TWSR & 0xF8) != STA_DATA_ACK)
 	{
-		PORTB |= (1<<LED1);
+		salida = TWI_ERROR;
 	}
 	
+	return salida;
 }
 
-void TWI_MT_Re_Start_Condition()
+/************************************************************************/
+/* REPEATED START: En el casi que queremos seguir enviando*/
+/************************************************************************/
+
+uint8_t TWI_MT_Re_Start_Condition(void)
 {
-	/************************************************************************/
-	/* REPEATED START */
-	/************************************************************************/
+	uint8_t salida = TWI_OK;
 	
 	//Repeated start
 	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTA);
@@ -144,35 +166,63 @@ void TWI_MT_Re_Start_Condition()
 	//Checkeamos si se env+ia el re-start
 	if((TWSR & 0xF8) != STA_RE_START)
 	{
-		PORTB |= (1<<LED1);
+		salida = TWI_ERROR;
 	}
+	
+	return salida;
 }
 
-void TWI_MT_Stop_Condition()
-{
+/************************************************************************/
+/* STOP CONDITION: Detener el periférico TWI*/
+/************************************************************************/
 
-	/************************************************************************/
-	/* STOP */
-	/************************************************************************/
-		
+void TWI_MT_Stop_Condition(void)
+{
+	
 	//Limpiamos la bandera y seguimos con el STOP condition
 	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWSTO);
 	
 }
 
-void TWI_MT_Write_Data(uint8_t address, uint8_t n_data)
+uint8_t TWI_MT_Write_Data(uint8_t address, uint8_t n_data, uint8_t Trama[])
 {
 	uint8_t i = 0;
+	uint8_t salida = TWI_OK;
 	
-	TWI_MT_Start_Condition();
-	
-	TWI_MT_Address(address,0);
-	
-	for(i = 0; i<n_data; i++)
+	//Asignamos la salida al resultado del START CONDITION
+	salida = TWI_MT_Start_Condition();
+	//Evaluamos el start condition
+	if(salida == TWI_ERROR)
 	{
-		TWI_MT_Data()
+		salida = TWI_ERROR;
 	}
 	
+	//Asignamos la salida al resultado de enviar el ADDRESS + R/nW
+	salida = TWI_MT_Address(address,0);
+	//Evaluamos el start condition
+	if(salida == TWI_ERROR)
+	{
+		salida = TWI_ERROR;
+	}
+	
+	//Transitiendo data
+	for(i = 0; i<n_data; i++)
+	{
+		salida = TWI_MT_Data_Upload(Trama[i]);
+		if(salida == TWI_ERROR)
+		{
+			break;
+		}
+	}
+	
+	if(salida == TWI_ERROR)
+	{
+		salida = TWI_ERROR;
+	}
+	
+	//Deteniendo el I2C
 	TWI_MT_Stop_Condition();
+	
+	return salida;
 	
 }
